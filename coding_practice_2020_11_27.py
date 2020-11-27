cadc436932a9f9bdf15a6b368923b336d2e017e9
YanChan9632 <74619519+YanChan9632@users.noreply.github.com>
2020-11-27 14:02:30 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,50 @@
+i = -188
+while i <= -158:
+    print(i)
+    i = i + 3
+
+i = -12
+while i <= -108:
+    print(i)
+    i = i + 4   
+
+i = 4
+while i <= 641:
+    print(i)
+    i = i + 13
+
+i = -412
+while i <= 478:
+    print(i)
+    i = i + 6
+
+i = 309
+while i <= 512:
+    print(i)
+    i = i + 7
+
+i = -256
+while i <= -60:
+    print(i)
+    i = i + 7
+
+i = 62
+while i <= 657:
+    print(i)
+    i = i + 17
+
+i = 256
+while i <= 412:
+    print(i)
+    i = i + 13
+
+i = 246
+while i <= 870:
+    print(i)
+    i = i + 13
+
+i = 151
+while i <= 448:
+    print(i)
+    i = i + 9
+
